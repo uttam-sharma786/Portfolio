28d3a2e3de3992df3492feb512027b0c9854c8af
uttam-sharma786 <uttamsharma.8560@gmail.com>
2024-04-03 22:58:33 +0530

    working on Home page

---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file
+// you can import this data from anywhere in your site by using the `import` keyword
+
+export const SITE_TITLE="Uttam_Portfolio";
+export const SITE_DESCRIPTION="Welcom to my Protfolio";
+export const SITE_LOGO = "/uttam.png";
