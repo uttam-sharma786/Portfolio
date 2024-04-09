77014e86ead69aa8a199920edb0a2627bc697f35
uttam-sharma786 <uttamsharma.8560@gmail.com>
2024-04-10 00:22:15 +0530

    "edit all page"

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,6 @@ const toggleMenu = () => {
     setISMenuOpen(!isMenuOpen())
 }
 
-
 const Navbar: Component = () => {
     return (
         <div class="flex justify-center bg-zinc-700 rounded-full sticky top-4 mb-4 backdrop-filter backdrop-blur-lg bg-opacity-30 w-full max-w-[400px] mx-auto">
@@ -16,8 +15,8 @@ const Navbar: Component = () => {
         <nav id = "menu" class="hidden md:flex">
             <a href="/." class="text-white px-4 py-2 md:inline">Home</a>
             <a href="/about" class="text-white px-4 py-2 md:inline">About</a>
-            <a href="#" class="text-white px-4 py-2  md:inline">Services</a>
-            <a href="#" class="text-white px-4 py-2 md:inline">Contact</a>
+            <a href="#" class="text-white px-4 py-2  md:inline">Pojects</a>
+            <a href="#" class="text-white px-4 py-2 md:inline">Blogs    </a>
 
         </nav>
      
