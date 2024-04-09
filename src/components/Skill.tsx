6bff47ea3e46f14b7977aee6ec9e519f337afd8d
uttam-sharma786 <uttamsharma.8560@gmail.com>
2024-04-10 00:03:13 +0530

    "Response done"

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -3,29 +3,22 @@ import type { Component } from "solid-js";
 
 const Skill: Component = () => {
     return (
-        <div class=" container  bg-zinc-700 px-2 sm:px-6 py-4 sm:py-12  rounded-md backdrop-filter backdrop-blur-lg bg-opacity-30">
-           
-            <ul class= "flex gap-6 ">
-                <li >
-                    <img src ="/public/Tailwind_CSS_Logo.svg.png" alt="Tailwind" class=" max-h-20 w-full object-contain items-center lg:col-span-1 "  />
-                </li>
-                <li >
-                    <img src ="/public/JavaScript-logo.png" alt = "JavaScript" class=" max-h-20  w-full object-contain items-center lg:col-span-1 "  />
-                </li>
-                <li>
-
-                    <img src ="/public/React-icon.svg.png"  alt = "React" class=" max-h-20 w-full object-contain items-center lg:col-span-3 "  />
-                </li>
-                <li>
-                    <img src ="/public/astrojs.svg" alt = "astro" class=" max-h-20 w-full object-contain items-center lg:col-span-1 " />
-                </li>
-            
-            </ul>
-            
-  
-       
-        
-        </div>
+    <div class="container mx-auto bg-zinc-700 px-6 sm:px-12 py-8 sm:py-16 rounded-md backdrop-filter backdrop-blur-lg bg-opacity-30">
+  <ul class="flex flex-row sm:flex-row justify-center items-center gap-6">
+    <li class="flex justify-center">
+      <img src="/public/Tailwind_CSS_Logo.svg.png" alt="Tailwind" class="max-h-20 w-full object-contain" />
+    </li>
+    <li class="flex justify-center">
+      <img src="/public/JavaScript-logo.png" alt="JavaScript" class="max-h-20 w-full object-contain" />
+    </li>
+    <li class="flex justify-center">
+      <img src="/public/React-icon.svg.png" alt="React" class="max-h-20 w-full object-contain" />
+    </li>
+    <li class="flex justify-center">
+      <img src="/public/astrojs.svg" alt="astro" class="max-h-20 w-full object-contain" />
+    </li>
+  </ul>
+</div> 
 
         
         
