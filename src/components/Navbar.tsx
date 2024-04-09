e99b5e7edb443580e152e09a1a5277c7c9ee7d60
uttam-sharma786 <uttamsharma.8560@gmail.com>
2024-04-09 23:51:19 +0530

    "done and responsive reamin"

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,29 @@
 import type { Component } from "solid-js";
+import { createSignal } from "solid-js";
+
+
+const [isMenuOpen, setISMenuOpen] = createSignal(false);
+
+const toggleMenu = () => {
+    setISMenuOpen(!isMenuOpen())
+}
 
 
 const Navbar: Component = () => {
     return (
-<div class="flex justify-center bg-zinc-700 rounded-full  sticky    w-[70%]  top-0  backdrop-filter backdrop-blur-lg bg-opacity-30">  
-        <nav class="flex">
+        <div class="flex justify-center bg-zinc-700 rounded-full sticky top-4 mb-4 backdrop-filter backdrop-blur-lg bg-opacity-30 w-full max-w-[400px] mx-auto">
+        <button id = "menuBtn" class="text-white px-4 py-2 md:hidden" onClick={toggleMenu}>Menu</button>
+        <nav id = "menu" class="hidden md:flex">
             <a href="/." class="text-white px-4 py-2 md:inline">Home</a>
             <a href="/about" class="text-white px-4 py-2 md:inline">About</a>
             <a href="#" class="text-white px-4 py-2  md:inline">Services</a>
             <a href="#" class="text-white px-4 py-2 md:inline">Contact</a>
-            {/* <button class="block md:hidden text-white px-4 py-2">Menu</button> */}
 
         </nav>
+     
     </div>
 
-        
-        
+ 
     );
 
 };
